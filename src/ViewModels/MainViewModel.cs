6c0715e709e9cb71730a1b83202ce7385f185636
Indra Yee <indra@Indras-Mac-mini.home>
2026-05-02 01:18:12 -0700

    Remove duplicate HotkeyDialog in Dialogs/ to fix build conflict

--- src/ViewModels/MainViewModel.cs
+++ src/ViewModels/MainViewModel.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Windows.Media;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using KeystrokeCommander;
 using KeystrokeCommander.Models;
 using KeystrokeCommander.Services;
 
